999cadfdeb5557e38608697636dc9a3bf1c5f383
ZhongHao-FS <75271488+ZhongHao-FS@users.noreply.github.com>
2021-09-05 20:49:59 -0400

    Create Xcode Project

---
+++ Pocket-Chef/Pocket-Chef/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pocket-Chef
+//
+//  Created by Hao Zhong on 9/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
