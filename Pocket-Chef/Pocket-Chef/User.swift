687e2819af185d3b5bd415e67c81c376624adaed
ZhongHao-FS <75271488+ZhongHao-FS@users.noreply.github.com>
2021-09-14 16:54:20 -0400

    Creating user class and settings screen

---
+++ Pocket-Chef/Pocket-Chef/User.swift
@@ -0,0 +1,26 @@
+//
+//  User.swift
+//  Pocket-Chef
+//
+//  Created by Hao Zhong on 9/14/21.
+//
+
+import Foundation
+
+class User {
+    // Stored Properties
+    let user_ID: Int
+    var name: String
+    var email: String
+    var password: String
+    var fav_Recipes = [Recipe]()
+    var shoppingList = [String]()
+    
+    // Initializer
+    init(userName: String, email: String, pwd: String) {
+        self.user_ID = 0
+        self.name = userName
+        self.email = email
+        self.password = pwd
+    }
+}
