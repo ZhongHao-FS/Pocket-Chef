986c6ca200891f5cb53757b43fb81731ba6ad9bd
ZhongHao-FS <75271488+ZhongHao-FS@users.noreply.github.com>
2021-09-21 17:01:14 -0400

    recipe detail setup

---
+++ Pocket-Chef/Pocket-Chef/RecipeDetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  RecipeDetailViewController.swift
+//  Pocket-Chef
+//
+//  Created by Hao Zhong on 9/21/21.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    var recipeID = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
